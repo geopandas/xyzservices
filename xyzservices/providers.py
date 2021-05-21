656df8cea5902cda8072c6bb0c828b0f47bd43ae
Martin Fleischmann <martin@martinfleischmann.net>
2021-05-21 17:39:14 +0100

    comment

--- xyzservices/providers.py
+++ xyzservices/providers.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from .lib import _load_json
 
+# we need to get an absolute path to our JSON
 JSON_PATH = (
     str(Path(__file__).parent.parent)
     + "/provider_sources/leaflet-providers-parsed.json"
