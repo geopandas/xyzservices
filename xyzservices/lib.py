aba77a6cde20c63bc1434e367722d5cd5a85f016
Martin Fleischmann <martin@martinfleischmann.net>
2021-05-21 17:05:25 +0100

    Parse JSON on import (#8)
    * json
    
    * parse json on the fly
    
    * Delete _providers.py

--- xyzservices/lib.py
+++ xyzservices/lib.py
@@ -1,6 +1,8 @@
 """
 Utilities to support XYZservices
 """
+import json
+
 
 class Bunch(dict):
     """A dict with attribute-access"""
@@ -26,3 +28,23 @@ class TileProvider(Bunch):
         new.update(kwargs)
         return new
 
+
+def _load_json(path):
+
+    with open(path, "r") as f:
+        data = json.load(f)
+
+    providers = Bunch()
+
+    for provider_name in data.keys():
+        provider = data[provider_name]
+
+        if "url" in provider.keys():
+            providers[provider_name] = TileProvider(provider)
+
+        else:
+            providers[provider_name] = Bunch(
+                {i: TileProvider(provider[i]) for i in provider}
+            )
+
+    return providers
