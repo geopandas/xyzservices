cfd9a69e18b63bb9056eed7d57aa8067060c5d5c
Martin Fleischmann <martin@martinfleischmann.net>
2021-05-21 17:38:32 +0100

    fix json loading

--- xyzservices/providers.py
+++ xyzservices/providers.py
@@ -1,5 +1,9 @@
+from pathlib import Path
 from .lib import _load_json
 
-JSON_PATH = "provider_sources/leaflet-providers-parsed.json"
+JSON_PATH = (
+    str(Path(__file__).parent.parent)
+    + "/provider_sources/leaflet-providers-parsed.json"
+)
 
 providers = _load_json(JSON_PATH)
