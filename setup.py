923a184ab62d7311570874126f56b79328bc0dd0
Martin Fleischmann <martin@martinfleischmann.net>
2021-05-21 14:01:54 +0100

    copy contextily implementation (#2)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="xyzservices",
+    version="0.0.0",
+    url="https://github.com/geopandas/xyzservices",
+    author="Dani Arribas-Bel",
+    author_email="daniel.arribas.bel@gmail.com",
+    license="3-Clause BSD",
+    packages=["xyzservices"],
+    python_requires=">=3.6",
+)
