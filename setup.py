c624d18c4e46a7d55d1a36890cffa79aff2e219f
Martin Fleischmann <martin@martinfleischmann.net>
2021-05-25 10:59:00 +0100

    Use versioneer, expand descriptions (#19)
    * Update .gitignore
    
    * expand info
    
    * use versioneer
    
    * inlcude license in MANIFEST

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
 import setuptools
+import versioneer
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="xyzservices",
-    version="0.0.0",
+    description="Source of XYZ tiles providers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/geopandas/xyzservices",
-    author="Dani Arribas-Bel",
-    author_email="daniel.arribas.bel@gmail.com",
+    author="Dani Arribas-Bel, Martin Fleischmann",
+    author_email="daniel.arribas.bel@gmail.com, martin@martinfleischmann.net",
     license="3-Clause BSD",
     packages=setuptools.find_packages(),
     python_requires=">=3.7",
     package_data={
         "xyzservices": ["provider_sources/leaflet-providers-parsed.json"],
     },
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
 )
--- xyzservices/__init__.py
+++ xyzservices/__init__.py
@@ -1,2 +1,6 @@
 from .providers import providers
 from .lib import Bunch, TileProvider
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
