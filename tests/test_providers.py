4bf50d0354b804fce71c36ef451f8dffff7f5ea1
Martin Fleischmann <martin@martinfleischmann.net>
2021-06-08 23:09:54 +0100

    ENH: add TileProvider.build_url()

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -1,6 +1,7 @@
 import pytest
 
 import xyzservices.providers as xyz
+from xyzservices import TileProvider
 
 
 def check_provider(provider):
@@ -21,3 +22,44 @@ def test_minimal_provider_metadata(provider_name):
     else:
         for variant in xyz[provider_name]:
             check_provider(xyz[provider_name][variant])
+
+
+def test_build_url():
+    basic = TileProvider(
+        {
+            "url": "https://myserver.com/tiles/{z}/{x}/{y}.png",
+            "attribution": "(C) xyzservices",
+            "name": "my_public_provider",
+        }
+    )
+    retina = TileProvider(
+        {
+            "url": "https://myserver.com/tiles/{z}/{x}/{y}{r}.png",
+            "attribution": "(C) xyzservices",
+            "name": "my_public_provider",
+            "r": "@2x",
+        }
+    )
+
+    silent_retina = TileProvider(
+        {
+            "url": "https://myserver.com/tiles/{z}/{x}/{y}{r}.png",
+            "attribution": "(C) xyzservices",
+            "name": "my_public_provider",
+        }
+    )
+
+    expected = "https://myserver.com/tiles/{z}/{x}/{y}.png"
+    assert basic.build_url() == expected
+
+    expected = "https://myserver.com/tiles/3/1/2.png"
+    assert basic.build_url(1, 2, 3) == expected
+    assert basic.build_url(1, 2, 3, scale_factor="@2x") == expected
+    assert silent_retina.build_url(1, 2, 3) == expected
+
+    expected = "https://myserver.com/tiles/3/1/2@2x.png"
+    assert retina.build_url(1, 2, 3) == expected
+    assert silent_retina.build_url(1, 2, 3, scale_factor="@2x") == expected
+
+    expected = "https://myserver.com/tiles/3/1/2@5x.png"
+    assert retina.build_url(1, 2, 3, scale_factor="@5x") == expected
--- xyzservices/lib.py
+++ xyzservices/lib.py
@@ -28,6 +28,59 @@ class TileProvider(Bunch):
         new.update(kwargs)
         return new
 
+    def build_url(self, x=None, y=None, z=None, scale_factor=None):
+        """
+        Build the URL of tiles from the TileProvider object
+
+        Can return URL with placeholders or the final tile URL.
+
+        Attributes
+        ----------
+
+        x, y, z : int
+            tile number
+        scale_factor : str
+            Scale factor (where supported). For example, you can get double resolution
+            (512 x 512) instead of standard one (256 x 256) with ``"@2x"``
+
+        Returns
+        -------
+
+        url : str
+
+        Examples
+        --------
+        >>> import xyzservices.providers as xyz
+
+        >>> xyz.CartoDB.DarkMatter.build_url()
+        'https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
+
+        >>> xyz.CartoDB.DarkMatter.build_url(x=9, y=11, z=5)
+        'https://a.basemaps.cartocdn.com/dark_all/5/9/11.png'
+
+        >>> xyz.CartoDB.DarkMatter.build_url(x=9, y=11, z=5, scale_factor="@2x")
+        'https://a.basemaps.cartocdn.com/dark_all/5/9/11@2x.png'
+
+        """
+        provider = self.copy()
+
+        if not x:
+            x = "{x}"
+        if not y:
+            y = "{y}"
+        if not z:
+            z = "{z}"
+
+        url = provider.pop("url")
+        subdomains = provider.pop("subdomains", "abc")
+        if scale_factor:
+            r = scale_factor
+            provider.pop("r", None)
+        else:
+            r = provider.pop("r", "")
+
+        return url.format(x=x, y=y, z=z, s=subdomains[0], r=r, **provider)
+
 
 def _load_json(f):
 
