638c0cfcfaad12b32c404b02b7babeff7a4cb665
Martin Fleischmann <martin@martinfleischmann.net>
2021-08-15 16:01:29 +0100

    check for empty

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -84,16 +84,18 @@ def test_free_providers(name):
 
 
 # test providers requiring API keys. Store API keys in GitHub secrets and load them as
-# environment variables in CI Action
+# environment variables in CI Action. Note that env variable is loaded as empty on PRs
+# from a fork.
 
 
 @pytest.mark.parametrize("provider_name", xyz.Thunderforest)
 def test_thunderforest(provider_name):
     try:
         token = os.environ["THUNDERFOREST"]
-        print(token, type(token))
     except KeyError:
         pytest.xfail("Mising API token.")
+    if token == "":
+        pytest.xfail("Token empty.")
 
     provider = xyz.Thunderforest[provider_name](apikey=token)
     get_test_result(provider, allow_403=False)
