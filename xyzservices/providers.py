4cf1635e34820a9e645041a24ec40261fef5c993
Martin Fleischmann <martin@martinfleischmann.net>
2023-02-19 13:38:32 +0100

    REF: use pkgutil in fallback read of JSON (#137)
    * REF: use pkgutil in fallback read of JSON
    
    * update flake8 pre-commit repo
    
    * lint
    
    * test fallback
    
    * fix condition
    
    * simplify gha yaml

--- xyzservices/providers.py
+++ xyzservices/providers.py
@@ -1,8 +1,7 @@
-import importlib.resources
 import os
+import pkgutil
 import sys
 
-from . import data
 from .lib import _load_json
 
 data_path = os.path.join(sys.prefix, "share", "xyzservices", "providers.json")
@@ -11,6 +10,6 @@ if os.path.exists(data_path):
     with open(data_path, "r") as f:
         json = f.read()
 else:
-    json = importlib.resources.read_text(data, "providers.json")
+    json = pkgutil.get_data("xyzservices", "data/providers.json")
 
 providers = _load_json(json)
