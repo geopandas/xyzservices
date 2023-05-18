c2a9b4dbf224c186fe74a6b3921479cce22c2b10
Martin Fleischmann <martin@martinfleischmann.net>
2023-05-18 10:19:19 +0200

    TST: skip Stadia tiles in tests

--- xyzservices/tests/test_providers.py
+++ xyzservices/tests/test_providers.py
@@ -102,6 +102,8 @@ def test_minimal_provider_metadata(provider_name):
 @pytest.mark.parametrize("name", flat_free)
 def test_free_providers(name):
     provider = flat_free[name]
+    if "Stadia" in name:
+        pytest.skip("Stadia doesn't support tile download in this way.")
     get_test_result(provider)
 
 
