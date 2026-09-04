0b6c12b342e108972f7f30d8a9206638457ec385
Martin Fleischmann <martin@martinfleischmann.net>
2026-09-04 11:20:17 +0200

    lint

--- xyzservices/tests/test_providers.py
+++ xyzservices/tests/test_providers.py
@@ -248,6 +248,7 @@ def test_os(provider_name):
 # NOTE: AzureMaps are not tested as their free account is limited to
 # 5000 downloads (total, not per month)
 
+
 @pytest.mark.request
 @pytest.mark.parametrize("provider_name", xyz.CartoDB)
 def test_carto(provider_name):
